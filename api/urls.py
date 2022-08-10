e386d208f40004a649d66bd9fc2b2a57563208b8
2044smile <2044smile@naver.com>
2022-08-11 08:55:01 +0900

    feat: urls

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from api.views import crawling_university, crawling_country
+
+urlpatterns = [
+    path('cwl_country/', crawling_country),
+    path('cwl_university/', crawling_university),
+]
