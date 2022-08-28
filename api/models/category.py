7c3dc1fcf13aa679b18b216f2f76387f37f0b193
2044smile <2044smile@naver.com>
2022-08-28 18:22:44 +0900

    refactor: add __str__

--- api/models/category.py
+++ api/models/category.py
@@ -5,7 +5,13 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     # category = models.ForeignKey('Menu', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"Category {self.name}"
+
 
 class SubCategory(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SubCategory {self.name}"
--- api/models/product.py
+++ api/models/product.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
     # label = models.ManyToManyField('Label', through='ProductLabel')
+
+    def __str__(self):
+        return f"Product {self.name}"
