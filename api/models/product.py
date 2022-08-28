780a711850c7cd385dc01796e2759c9892408fee
2044smile <2044smile@naver.com>
2022-08-28 18:02:14 +0900

    refactor: not use label

--- api/models/product.py
+++ api/models/product.py
@@ -5,4 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     sub_category = models.ForeignKey('SubCategory', on_delete=models.CASCADE)
-    label = models.ManyToManyField('Label', through='ProductLabel')
+    # label = models.ManyToManyField('Label', through='ProductLabel')
